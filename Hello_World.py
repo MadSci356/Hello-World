4baeb249c254ba2a2204a726158bfca0b6753896
Sayam <patel.sayam@ymail.com>
2013-09-08 12:43:07 -0500

    Gotcha!

--- Hello_World.py
+++ Hello_World.py
@@ -2,9 +2,8 @@ import time
 print "Hello..."
 time.sleep(5)
 x = 0
-while x <=5:
+while x <=10:
     print "SURPRISE MUTHAFUCKA!"
     print ''
-    x += 1
-
-time.sleep(3)
+    x += 2
+time.sleep(2)
